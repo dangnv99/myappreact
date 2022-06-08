3032685d7c1b94e141f58d7b76eb990d98924fb3
Dangnv <dangphucnguyen0309@gmail.com>
2022-06-08 17:17:24 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useReducer } from 'react'
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '0 20px', margin: '1rem' }}>
+      <h1 style={{ padding: '0 20px', margin: '1rem' }} >{count}</h1>
+      <button style={{ padding: '0 20px', margin: '1rem' }} onclick={() => setCount(count - 1)}> Down</button>
+      <button style={{ padding: '0 20px', margin: '1rem' }} onclick={() => setCount(count + 1)}> Up</button>
     </div>
   );
 }
